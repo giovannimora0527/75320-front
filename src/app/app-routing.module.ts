e1eaebdf98efdac1419fe8768deeaf1a964b0e34
MiguelGarcia <mgarciadiaz5a@gmail.com>
2025-04-06 11:13:12 -0500

    Agrego componentes y servicios de libros y prestamos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,8 @@ import { Routes, RouterModule } from '@angular/router';
 import { AdminComponent } from './theme/layout/admin/admin.component';
 import { UsuarioComponent } from './demo/pages/usuario/usuario.component';
 import { AutorComponent } from './demo/pages/autor/autor.component';
+import { LibrosComponent } from 'src/app/demo/pages/libros/libros.component';
+import { PrestamosComponent } from 'src/app/demo/pages/prestamos/prestamos.component';
 
 export const routes: Routes = [
   {
@@ -16,7 +18,9 @@ export const routes: Routes = [
     data: { title: 'Inicio' },
     children: [      
       { path: 'usuarios', component: UsuarioComponent, data: { title: 'Usuarios' }},
-      { path: 'autores', component: AutorComponent, data: { title: 'Autores' }}     
+      { path: 'autores', component: AutorComponent, data: { title: 'Autores' }},  
+      { path: 'libros', component: LibrosComponent },
+      { path: 'prestamos', component: PrestamosComponent },  
     ]
   },
   { path: '**', redirectTo: 'inicio' }
@@ -27,3 +31,4 @@ export const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule {}
+
--- src/app/theme/layout/admin/navigation/navigation.ts
+++ src/app/theme/layout/admin/navigation/navigation.ts
@@ -36,6 +36,22 @@ export const NavigationItems: NavigationItem[] = [
         url: '/inicio/autores',
         icon: 'feather icon-users',
         classes: 'nav-item'
+      },
+            {
+        id: 'libros',
+        title: 'Gestión de Libros',
+        type: 'item',
+        url: '/inicio/libros',
+        icon: 'feather icon-book',
+        classes: 'nav-item'
+      },
+      {
+        id: 'prestamos',
+        title: 'Gestión de Préstamos',
+        type: 'item',
+        url: '/inicio/prestamos',
+        icon: 'feather icon-repeat',
+        classes: 'nav-item'
       }
     ]
   },
