255b1b9985fa0eaa9d97a606aa9ca5338d4e800d
HelenGallo <helentatiana03@hotmail.com>
2025-04-06 22:07:59 -0500

    Actualización del proyecto con cambios en en front

---
+++ src/app/demo/pages/libros/libros.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  standalone: true,
+  selector: 'app-libros',
+  imports: [CommonModule], // Aquí agregas más si usas FormsModule, RouterModule, etc.
+  templateUrl: './libros.component.html',
+  styleUrls: ['./libros.component.scss']
+})
+export class LibrosComponent {}
---
+++ src/app/demo/pages/prestamos/prestamos.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  standalone: true,
+  selector: 'app-prestamos',
+  imports: [CommonModule],
+  templateUrl: './prestamos.component.html',
+  styleUrls: ['./prestamos.component.scss']
+})
+export class PrestamosComponent {}
+
